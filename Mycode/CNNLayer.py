db41085bd6a2d6bf64c56d884d91a403e9315ac9
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-18 23:56:14 +0700

    Modified:
    - Create filter, conv, pool layer

--- Mycode/CNNLayer.py
+++ Mycode/CNNLayer.py
@@ -1,37 +1,51 @@
 import theano
 import theano.tensor
+import numpy
 from theano.tensor.nnet import conv2d
 from theano.tensor.signal.pool import pool_2d
-
+from pylab import *
 
 class CNNLayer:
     def __init__(self,
-                 learningRate,
-                 imageShape,
+                 rng,
+                 input,
+                 inputShape,
                  filterShape,
                  poolingShape = (2, 2)):
         # Set parameters
-        self.LearningRate = learningRate
-        self.ImageShape = imageShape
+        self.Input = input
+        self.InputShape = inputShape
         self.FilterShape = filterShape
         self.PoolingShape = poolingShape
 
-        # Create convolution layer
-        convLayer = conv2d(
-            input = inputConv,
-            input_shape = imageShape,
-            filters = filtersConv,
-            filter_shape = filterShape,
-            border_mode =  'full'
+        # Calculate filters size
+
+
+        # Create filter
+        self.filters = theano.shared(
+            numpy.asarray(
+                rng.uniform(low = ..., high = ..., size = self.FilterShape),
+                dtype = theano.config.floatX
+            ),
+            borrow = True
         )
 
+        # Create convolution layer
+        outputConv = convLayer = conv2d(
+            input = self.Input,
+            input_shape = self.InputShape,
+            filters = ,
+            filter_shape = self.FilterShape
+        )
 
         # Create pooling layer
-        poolLayer = pool_2d(
-            input = inputData,
-            ds = poolingShape,
-            mode = 'max',
+        poolOut = pool_2d(
+            input = outputConv,
+            ds = self.PoolingShape,
             ignore_border = True
         )
 
-        
+        self.Output = theano.tensor.tanh(poolOut + )
+
+        self.Params = [self.W, self.b]
+
