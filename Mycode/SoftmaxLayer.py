16ef26b2defdd14d5ef9a78f16b198183c4428c4
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-24 14:25:09 +0700

    Del:
    - Remove unused files

--- Mycode/SoftmaxLayer.py
+++
@@ -1,9 +0,0 @@
-from theano.tensor.nnet.nnet import softmax
-
-class SoftmaxLayer:
-    def __init__(self,
-                 input):
-        self.Input = input
-
-        # Create layer
-        self.Output = softmax(self.Input)
