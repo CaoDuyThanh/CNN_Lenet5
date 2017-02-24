4f065d499f9e3b6da49b986f1cf3dda2ffb65e6e
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-24 11:19:06 +0700

    Modified:
    - Add pooling layer

--- Layers/ConvPoolLayer.py
+++ Layers/ConvPoolLayer.py
@@ -10,7 +10,8 @@ class ConvPoolLayer:
                  input,                 # Data
                  inputShape,            # Shape of input = [batch size, channels, rows, cols]
                  filterShape,           # Shape of filter = [number of filters, channels, rows, cols]
-                 poolingShape = (2, 2)  # Shape of pooling (2, 2) default
+                 poolingShape = (2, 2), # Shape of pooling (2, 2) default
+                 W = None
                  ):
         # Set parameters
         self.Input = input
@@ -19,24 +20,37 @@ class ConvPoolLayer:
         self.PoolingShape = poolingShape
 
         # Create shared parameters for filters
-        self.W = theano.shared(
-            input = numpy.asarray(
-                rng.uniform(
-                    low = -1.0,
-                    high = 1.0,
-                    size = self.FilterShape
+        if W is None:
+            self.W = theano.shared(
+                input = numpy.asarray(
+                    rng.uniform(
+                        low = -1.0,
+                        high = 1.0,
+                        size = self.FilterShape
+                    ),
+                    dtype = theano.config.floatX
                 ),
-                dtype = theano.config.floatX
-            ),
-            borrow = True
+                borrow = True
+            )
+        else:
+            self.W = W
+
+        convLayer = conv2d(
+            input = self.Input,
+            input_shape = (self.InputShape),
+            filters = self.W,
+            filter_shape = self.FilterShape,
         )
 
-
-
+        self.poolLayer = pool_2d(
+            input = convLayer,
+            ds = self.PoolingShape,
+            ignore_border = True
+        )
 
     def Params(self):
-        return 0
+        return [self.W]
 
 
     def Output(self):
-        return 0
+        return self.poolLayer
